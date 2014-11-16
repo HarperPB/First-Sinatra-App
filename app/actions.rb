0b3c69a43a943ef374e5fdd80424e54698c74a23
Harper <harper.price.brown@gmail.com>
2014-11-15 17:03:50 -0800

    added the comment functionality , Step 6

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,4 @@
-# Homepage (Root path)
+
 get '/' do
   erb :index
 end
@@ -6,4 +6,19 @@ end
 get '/messages' do
   @messages = Message.all
   erb :'messages/index'
+end
+binding.pry
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title:   params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  binding.pry
+  @message.save
+  redirect '/messages'
 end
